aff5eb81b68861d95b16edf065dfaf0cdb6f9a68
aliyeysides <ali.yeysides@gmail.com>
2014-12-06 18:40:07 -0500

    add background to manifest

--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
-chrome.runtime.onMessage({
-	
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+	console.log(message.phone)
+  // if (message.method === "add note"){
+  //   addNote(message.url, message.note)
+  // } else if (message.method === "remove note"){
+  //   removeNote(message.url, message.index)
+  // }
+  sendResponse(console.log("got it brah"))
 })
